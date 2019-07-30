46bbe14fa28a9b845cf16f863aa7a0bce9aa3dad
106 <Segmentation-fault@yandex.ua>
2019-07-30 18:01:08 +0900

    外部からインポートしやすくする

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-from mltools import *
+import os
+__path__ = [os.path.join(os.path.dirname(__file__), "mltools")]
+from .parameter import parameter
+from . import optimizer, data
--- mltools/__init__.py
+++
@@ -1,1 +0,0 @@
-from mltools.parameter import parameter
