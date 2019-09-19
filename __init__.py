66470e45b24363c26ad01a47561fdd72aba55de6
106 <Segmentation-fault@yandex.ua>
2019-09-19 17:51:49 +0900

    エポック計算機の追加

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@ import os
 __path__ = [os.path.join(os.path.dirname(__file__), "mltools")]
 from .parameter import Parameter
 from .learninglog import LearningLog
+from .epochcalc import EpochCalc
 from . import optimizer, data
